631576507ef6455917acc1ab4736e472c041e158
Sultan <sultanyelikbayev@gmail.com>
2023-04-22 17:53:03 +0600

    testing of JsonCamelCase function was added

--- internal/casing/camel_test.go
+++ internal/casing/camel_test.go
@@ -33,11 +33,54 @@ func TestCamelIdentifier(t *testing.T) {
 			"path.pathenum.snake_case_0_enum",
 			"path.pathenum.SnakeCase_0Enum",
 		},
+		{
+			"empty string identifier",
+			"",
+			"",
+		},
+		{
+			"snake case identifier started with underscore",
+			"_snake_case_0_enum",
+			"XSnakeCase_0Enum",
+		},
 	}
 
 	for _, ct := range casingTests {
 		t.Run(ct.name, func(t *testing.T) {
 			got := CamelIdentifier(ct.input)
+			if ct.want != got {
+				t.Errorf("want: %s, got: %s", ct.want, got)
+			}
+		})
+	}
+}
+
+func TestJsonCamelCase(t *testing.T) {
+	casingTests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			"regular snake case identifier",
+			"snake_case",
+			"snakeCase",
+		},
+		{
+			"snake case identifier with digit",
+			"snake_case_0_enum",
+			"snakeCase0Enum",
+		},
+		{
+			"snake case identifier with several underscores",
+			"snake__case_with__multiple____underscores",
+			"snakeCaseWithMultipleUnderscores",
+		},
+	}
+
+	for _, ct := range casingTests {
+		t.Run(ct.name, func(t *testing.T) {
+			got := JSONCamelCase(ct.input)
 			if ct.want != got {
 				t.Errorf("want: %s, got: %s", ct.want, got)
 			}
